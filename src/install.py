3c24053d475cd2ab8c47b0e8e38bb19c5d0167ff
RonTamG <33351836+RonTamG@users.noreply.github.com>
2023-03-15 06:57:28 +0000

    feat(install): added create install script function

---
+++ src/install.py
@@ -0,0 +1,13 @@
+def create_install_script(filenames):
+    '''
+    create a bash script that installs the package with all it's dependencies
+    '''
+    packages = [f'dpkg -i ./packages/{name}' for name in reversed(filenames)]
+
+    data = '#!/bin/bash\n\n'
+    data += 'cp ./update/* /var/lib/apt/lists/\n'
+    data += '\n'.join(packages)
+    data += '\n'
+    data += 'apt --fix-broken install -y'
+
+    return data
---
+++ tests/test_install.py
@@ -0,0 +1,24 @@
+from src.install import *
+
+
+def test_create_install_script():
+    filenames = ['libc6_2.31-13+deb11u5_amd64.deb',
+                 'libgcc-s1_10.2.1-6_amd64.deb',
+                 'libcrypt1_4.4.18-4_amd64.deb',
+                 'gcc-10-base_10.2.1-6_amd64.deb'
+                 ]
+
+    expected = '''\
+#!/bin/bash
+
+cp ./update/* /var/lib/apt/lists/
+dpkg -i ./packages/gcc-10-base_10.2.1-6_amd64.deb
+dpkg -i ./packages/libcrypt1_4.4.18-4_amd64.deb
+dpkg -i ./packages/libgcc-s1_10.2.1-6_amd64.deb
+dpkg -i ./packages/libc6_2.31-13+deb11u5_amd64.deb
+apt --fix-broken install -y\
+'''
+
+    result = create_install_script(filenames)
+
+    assert result == expected
