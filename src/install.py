d1f1755f4ecc212f0f41eb1ccb5d45708d318215
RonTamG <33351836+RonTamG@users.noreply.github.com>
2023-03-25 10:08:22 +0000

    feat(install): added refuse downgrade and no download

--- src/install.py
+++ src/install.py
@@ -2,12 +2,12 @@ def create_install_script(filenames):
     '''
     create a bash script that installs the package with all it's dependencies
     '''
-    packages = [f'dpkg -i ./packages/{name}' for name in reversed(filenames)]
+    packages = [f'dpkg --refuse-downgrade -i ./packages/{name}' for name in reversed(filenames)]
 
     data = '#!/bin/bash\n\n'
     data += 'cp ./update/* /var/lib/apt/lists/\n'
     data += '\n'.join(packages)
     data += '\n'
-    data += 'apt --fix-broken install -y'
+    data += 'apt --fix-broken install --no-download -y'
 
     return data
