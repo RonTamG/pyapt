625970566360352f61b8c8c4dbfd339645003892
RonTamG <33351836+RonTamG@users.noreply.github.com>
2023-07-31 18:24:39 +0000

    feat: added cleanup of install script

--- src/install.py
+++ src/install.py
@@ -5,11 +5,28 @@ def create_install_script(filenames):
     packages = [
         f"dpkg --refuse-downgrade -i ./packages/{name}" for name in reversed(filenames)
     ]
+    install_lines = "\n".join(packages)
 
-    data = "#!/bin/bash\n\n"
-    data += "cp ./update/* /var/lib/apt/lists/\n"
-    data += "\n".join(packages)
-    data += "\n"
-    data += "apt --fix-broken install --no-download -y"
+    script = f"""\
+#!/bin/bash
 
-    return data
+# save original lists
+mkdir /tmp/pyapt
+mv /var/lib/apt/lists/* /tmp/pyapt/
+
+# transfer required lists to directory
+cp ./update/* /var/lib/apt/lists/
+
+# install packages
+{install_lines}
+
+# fix installs to complete all dependencies
+apt --fix-broken install --no-download -y
+
+# cleanup and restore original lists
+rm /var/lib/apt/lists/*
+mv /tmp/pyapt/* /var/lib/apt/lists/
+rmdir /tmp/pyapt
+"""
+
+    return script
--- tests/test_install.py
+++ tests/test_install.py
@@ -12,12 +12,26 @@ def test_create_install_script():
     expected = """\
 #!/bin/bash
 
+# save original lists
+mkdir /tmp/pyapt
+mv /var/lib/apt/lists/* /tmp/pyapt/
+
+# transfer required lists to directory
 cp ./update/* /var/lib/apt/lists/
+
+# install packages
 dpkg --refuse-downgrade -i ./packages/gcc-10-base_10.2.1-6_amd64.deb
 dpkg --refuse-downgrade -i ./packages/libcrypt1_4.4.18-4_amd64.deb
 dpkg --refuse-downgrade -i ./packages/libgcc-s1_10.2.1-6_amd64.deb
 dpkg --refuse-downgrade -i ./packages/libc6_2.31-13+deb11u5_amd64.deb
-apt --fix-broken install --no-download -y\
+
+# fix installs to complete all dependencies
+apt --fix-broken install --no-download -y
+
+# cleanup and restore original lists
+rm /var/lib/apt/lists/*
+mv /tmp/pyapt/* /var/lib/apt/lists/
+rmdir /tmp/pyapt
 """
 
     result = create_install_script(filenames)
