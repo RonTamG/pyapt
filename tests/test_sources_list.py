7a1907720ef3565af049da23db7ed0cd64542a64
RonTamG <33351836+RonTamG@users.noreply.github.com>
2023-11-10 22:13:55 +0000

    test: refactored sources_list tests

--- tests/test_sources_list.py
+++ tests/test_sources_list.py
@@ -2,13 +2,35 @@ from src.sources_list import SourcesList
 
 
 def test_sources_list_returns_index_urls():
-    sources_list = """\
-        # deb http://deb.debian.org/debian focal main contrib non-free
-        deb http://deb.debian.org/debian bullseye main contrib non-free
-        deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-        deb http://security.debian.org/debian-security bullseye-security main contrib non-free
-        """  # noqa: E501
+    sources_list, expected = valid_sources_list()
 
+    result = SourcesList(sources_list).index_urls(architecture="amd64")
+
+    assert all(item in result for item in expected)
+
+
+def test_sources_list_with_commented_lines_ignores_them():
+    sources_list, expected = sources_list_with_commented_lines()
+
+    result = SourcesList(sources_list).index_urls(architecture="amd64")
+
+    assert all(item in result for item in expected)
+
+
+def test_sources_list_with_whitespace_ignores_it():
+    sources_list, expected = sources_list_with_whitespace()
+
+    result = SourcesList(sources_list).index_urls(architecture="amd64")
+
+    assert all(item in result for item in expected)
+
+
+def valid_sources_list():
+    sources_list = """\
+deb http://deb.debian.org/debian bullseye main contrib non-free
+deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+deb http://security.debian.org/debian-security bullseye-security main contrib non-free
+"""
     expected = [
         "http://deb.debian.org/debian/dists/bullseye/main/binary-amd64/Packages",
         "http://deb.debian.org/debian/dists/bullseye/contrib/binary-amd64/Packages",
@@ -20,19 +42,35 @@ def test_sources_list_returns_index_urls():
         "http://security.debian.org/debian-security/dists/bullseye-security/contrib/binary-amd64/Packages",
         "http://security.debian.org/debian-security/dists/bullseye-security/non-free/binary-amd64/Packages",
     ]
-
-    result = SourcesList(sources_list).index_urls(architecture="amd64")
-    assert result == expected
+    return sources_list, expected
 
 
-def test_sources_list_ignores_commented_lines():
+def sources_list_with_commented_lines():
     sources_list = """\
-    # deb http://deb.debian.org/debian focal main contrib non-free
-    ## deb http://deb.debian.org/debian bullseye main contrib non-free
-    deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-    deb http://security.debian.org/debian-security bullseye-security main contrib non-free
-    """  # noqa: E501
+#deb http://deb.debian.org/debian bullseye main contrib non-free
+# deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+deb http://security.debian.org/debian-security bullseye-security main contrib non-free
+"""
+    expected = [
+        "http://security.debian.org/debian-security/dists/bullseye-security/main/binary-amd64/Packages",
+        "http://security.debian.org/debian-security/dists/bullseye-security/contrib/binary-amd64/Packages",
+        "http://security.debian.org/debian-security/dists/bullseye-security/non-free/binary-amd64/Packages",
+    ]
 
+    return sources_list, expected
+
+
+def sources_list_with_whitespace():
+    sources_list = """\
+# deb http://deb.debian.org/debian focal main contrib non-free
+
+deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+## deb http://deb.debian.org/debian bullseye main contrib non-free
+
+
+deb http://security.debian.org/debian-security bullseye-security main contrib non-free
+
+"""
     expected = [
         "http://deb.debian.org/debian/dists/bullseye-updates/main/binary-amd64/Packages",
         "http://deb.debian.org/debian/dists/bullseye-updates/contrib/binary-amd64/Packages",
@@ -42,32 +80,4 @@ def test_sources_list_ignores_commented_lines():
         "http://security.debian.org/debian-security/dists/bullseye-security/non-free/binary-amd64/Packages",
     ]
 
-    result = SourcesList(sources_list).index_urls(architecture="amd64")
-    assert result == expected
-
-
-def test_sources_list_ignores_whitespace_lines():
-    sources_list = """\
-    # deb http://deb.debian.org/debian focal main contrib non-free
-
-    deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-    ## deb http://deb.debian.org/debian bullseye main contrib non-free
-
-
-    deb http://security.debian.org/debian-security bullseye-security main contrib non-free
-
-
-
-    """  # noqa: E501
-
-    expected = [
-        "http://deb.debian.org/debian/dists/bullseye-updates/main/binary-amd64/Packages",
-        "http://deb.debian.org/debian/dists/bullseye-updates/contrib/binary-amd64/Packages",
-        "http://deb.debian.org/debian/dists/bullseye-updates/non-free/binary-amd64/Packages",
-        "http://security.debian.org/debian-security/dists/bullseye-security/main/binary-amd64/Packages",
-        "http://security.debian.org/debian-security/dists/bullseye-security/contrib/binary-amd64/Packages",
-        "http://security.debian.org/debian-security/dists/bullseye-security/non-free/binary-amd64/Packages",
-    ]
-
-    result = SourcesList(sources_list).index_urls(architecture="amd64")
-    assert result == expected
+    return sources_list, expected
