267a354313b785e33f29991f9927cc38d54197b5
RonTamG <33351836+RonTamG@users.noreply.github.com>
2023-11-11 21:32:26 +0000

    ci: added git download and install test to ci

--- tests/acceptance_tests/install_git.sh
+++ tests/acceptance_tests/install_git.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # This test will download and install git using pyapt.
+if [ "$1" == "--no-progress-bar" ]; then
+    remove_progress_bar="--no-progress-bar"
+else
+    remove_progress_bar=""
+fi
 
 # remove git so we can install it later
 apt-get remove git -y && apt-get autoremove -y
 
 # download the package, it will create a file called git.tar.gz
-python main.py --sources-list /etc/apt/sources.list git 
+python main.py --sources-list /etc/apt/sources.list $remove_progress_bar git 
 
 # extract the contents of the file
 tar -xvf git.tar.gz
