fed6980176eba7431a48fedd5a7a0745c4ef68bd
RonTamG <33351836+RonTamG@users.noreply.github.com>
2023-12-09 20:48:28 +0000

    feat: added less than to version comparison

--- src/version.py
+++ src/version.py
@@ -17,6 +17,12 @@ class Version:
         else:
             raise NotImplementedError
 
+    def __lt__(self, other) -> bool:
+        if isinstance(other, Version):
+            return self._compare(other) <= -1
+        else:
+            raise NotImplementedError
+
     def _compare(self, other) -> int:
         """
         compare 2 debain version strings
--- tests/test_version.py
+++ tests/test_version.py
@@ -27,3 +27,22 @@ def test_should_compare_equal(a, b):
 )
 def test_should_compare_greater_than(a, b):
     assert Version(a) > Version(b)
+
+
+@pytest.mark.parametrize(
+    "a, b",
+    [
+        ("1:0", "2:0"),
+        ("0:1-1", "0:2-1"),
+        ("0:1-1", "0:1-2"),
+        ("0:0-0", "1:0-0"),
+        ("11.1+deb11u6", "1:11.1+deb11u6"),
+        ("0:a-0", "0:b-0"),
+        ("11.1+deb11u6", "11.2+deb11u6"),
+        ("0:0-a", "0:0-b"),
+        ("1.6.1-5", "1.6.1-5+deb11u1"),
+        ("1:16.28.0~dfsg-0+deb11u1", "1:16.28.0~dfsg-0+deb11u2"),
+    ],
+)
+def test_should_compare_less_than(a, b):
+    assert Version(a) < Version(b)
